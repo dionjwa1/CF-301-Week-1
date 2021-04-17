60b82a91486a7143a9aa50a8ade1b95aadb8b330
Dion Johnson <dionjwa@gmail.com>
2021-04-17 12:31:57 -0700

    start to forms

--- src/components/forms.js
+++ src/components/forms.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Dropdown, DropdownButton, Form } from 'react-bootstrap';
+class Forms extends React.Component {
 
-class Form extends React.Component {
   render() {
     return (
-      <input>
-        <form onSubmit={this.XXXX}>
-          <label>
-            Form data
-        <input type="text" value={this.XXXX} onSubmit={this.XXXX} />
-          </label>
-        </input>
-        </form>
+
+      <Form>
+        <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+          <Dropdown.Item eventKey ="1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </DropdownButton>
+      </Form>
     );
   }
 }
 
+export default Forms;
